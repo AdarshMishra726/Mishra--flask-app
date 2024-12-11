900faefd90931206c5fb49460b7682ad77f95e33
AdarshMishra726 <166946844+AdarshMishra726@users.noreply.github.com>
2024-12-10 21:41:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask, jsonify
-
+from flask import Flask
+ 
 app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return jsonify({"message": "Hello, World! Welcome to Adarsh's Flask App!"})
-
-@app.route('/health')
-def health():
-    return jsonify({"status": "healthy"})
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+ 
+@app.route("/")
+def hello():
+    return "Hello from Mishra ECS Container."
+ 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
